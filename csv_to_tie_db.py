b417b7ea56d238ccca03e0bd0ebb40018a58d296
fechan <edu.fechan@gmail.com>
2020-08-06 18:40:52 -0700

    create csv converter

---
+++ csv_to_tie_db.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""csv_to_tie_db.py: Adds rows from the travelers_in_egypt .csv file to the .sqlite3 SQL database
+"""
+
+import sqlite3
+import csv
