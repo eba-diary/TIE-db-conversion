17e01ec30ade0b5392aed91a58590af6125ae09a
fechan <edu.fechan@gmail.com>
2020-08-10 18:07:58 -0700

    add page header discriminator

--- parse_ocr_txt.py
+++ parse_ocr_txt.py
@@ -4,8 +4,10 @@
 """parse_ocr_txt.py: Parse raw ORC'd text of Kalfatovik's Nile Notes of a Howadji
 """
 
-FIRST_ENTRY_NBR = 789
-OCR_FILENAME = "raw_ocr.txt"
+import re
+
+FIRST_ENTRY_NBR = 789           # First entry number to parse. Used for error checking.
+OCR_FILENAME = "raw_ocr.txt"    # Filename of OCR text
 
 class Entry:
     """Represents an entry in the book"""
@@ -21,3 +23,20 @@ class Work:
     def __init__(self, title, annotation):
         self.title = title
         self.annotation = annotation
+
+def is_page_header(line):
+    """Return True if the given line of text is a page header
+    e.g. "278 Nile Notes of a Howadji" (even pages) or "1880-1889 - 279 " (odd pages)
+    """
+    even_page =  bool(re.match(r"(\d{1,3})( )(Nile Notes of a Howadji)( ?)", line))
+    odd_page =  bool(re.match(r"(\d{4}-\d{4})( )(\d{3})( ?)", line))
+    return even_page or odd_page
+
+ocr_file = open(OCR_FILENAME)
+ocr_lines = ocr_file.readlines()
+ocr_file.close()
+
+entries = []
+current_entry = FIRST_ENTRY_NBR
+for line in ocr_lines:
+    print(line)
