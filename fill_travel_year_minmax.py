3db9a97aa93c93a52979eb44bf9d4c1841df2819
fechan <edu.fechan@gmail.com>
2020-09-14 22:20:55 -0700

    use correct sanitizing regex

--- fill_travel_year_minmax.py
+++ fill_travel_year_minmax.py
@@ -16,3 +16,16 @@ db = connection.cursor()
 db.execute("SELECT id, travel_dates FROM publications")
 publications = db.fetchall()
 
+for publication in publications:
+    pub_id = publication[0]
+    datestring = publication[1]
+    if datestring == None: continue
+
+    datestring = re.sub(r"[^0-9 \-]", "", datestring).strip()
+    print(datestring)
+    dates = datestring.split()
+    if len(dates) == 1:
+        match = re.match(r"^[0-9]{3}-", dates[0])
+        if match:
+            partial_year = match.group(0)[:3].replace("-", "")
+        
