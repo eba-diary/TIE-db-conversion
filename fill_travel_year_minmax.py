67f65f77433ab5e71816fe5858360d11975c8f3d
fechan <edu.fechan@gmail.com>
2020-09-14 22:45:18 -0700

    move year exctration to own function

--- fill_travel_year_minmax.py
+++ fill_travel_year_minmax.py
@@ -8,7 +8,7 @@ the appropriate columns
 import sqlite3
 import re
 
-DATABASE_FILE = "travelogues.sqlite3"
+DATABASE_FILE = "travelogues.test.sqlite3"
 
 connection = sqlite3.connect(DATABASE_FILE)
 db = connection.cursor()
@@ -16,16 +16,34 @@ db = connection.cursor()
 db.execute("SELECT id, travel_dates FROM publications")
 publications = db.fetchall()
 
+def get_year(datetoken, get_min):
+    """ Get the min or max year from a date token from the database.
+        Tokens are a single year or range of two years.
+        datetoken - date token from the database.
+        get_min - gets the min if true, max if false
+    """
+    year = None
+    match = re.match(
+        r"(^[0-9]{3}-$)|(^[0-9]{4}\-$)|(^[0-9]{4}$)|(^[0-9]{4}-[0-9]{2}$|(^[0-9]{4}-[0-9]{4}$))",
+        datetoken
+        )
+    if match:
+        partial_year = match.group(0)[:3].replace("-", "")
+        year = partial_year + ("0" if get_min else "9")
+    return year
+
 for publication in publications:
     pub_id = publication[0]
     datestring = publication[1]
     if datestring == None: continue
 
     datestring = re.sub(r"[^0-9 \-]", "", datestring).strip()
-    print(datestring)
     dates = datestring.split()
-    if len(dates) == 1:
-        match = re.match(r"^[0-9]{3}-", dates[0])
-        if match:
-            partial_year = match.group(0)[:3].replace("-", "")
+    min_year = get_year(dates[0], True)
+    max_year = get_year(dates[-1], False)
+    
         
+    db.execute("""UPDATE publications
+                  SET travel_year_min = ?, travel_year_max = ?
+                  WHERE id = ?""", (min_year, max_year, pub_id))
+connection.commit()
